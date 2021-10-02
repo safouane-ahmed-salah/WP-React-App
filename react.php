cf9c818faf068e0ddd9438b031c684d9465b5089
Safwan Ahmed Salah <safwan@caspiands.com>
2021-10-02 12:42:32 +0800

    save assets to meta field

--- react.php
+++ react.php
@@ -25,6 +25,30 @@ function react_create_post_type() {
     register_post_type('react', $args);
 }
 
+//get assets
+add_action( 'template_redirect', 'react_template_redirect' );
+function react_template_redirect(){
+  global $wp;
+  if(preg_match('@^react/([\w-]+)/assets/(.*)@', $wp->request, $m)){
+    $slug = @$m[1];
+    $file = @$m[2];
+    $post = get_page_by_path($slug, OBJECT,'react');
+    if($post){
+      $assets= get_post_meta($post->ID, 'assets', true);
+      $mime = wp_check_filetype($file);
+      $content = @$assets[$file];
+
+      if($content){
+        status_header(200);
+        header('Content-Type: '. $mime['type']);
+        echo base64_decode($content);
+        exit;
+      }
+    }
+    
+  }
+}
+
 add_filter('single_template', 'react_template'); 
 function react_template() {
     global $post;
@@ -35,21 +59,6 @@ function react_template() {
     }
 
     return $single;
-}
-
-//functions
-function react_get_build_dir($postId, $key = 'basedir', $path = '/build'){
-  $upload_dir = wp_upload_dir();
-  return $upload_dir[$key].'/reactapp/'.$postId.$path;
-}
-function react_get_manifest($id, $key){
-  $manifest_file = react_get_build_dir($id).'/asset-manifest.json';
-  if(!file_exists($manifest_file)) return;
-  $manifest = file_get_contents($manifest_file);
-  $json = json_decode($manifest, true);
-  $files = $json['files'];
-
-  return @$files[$key];
 }
 
 function react_get_shortcode($id){
@@ -64,12 +73,19 @@ function react_get_html_id($id){
 add_shortcode('react-app', 'react_shortcode');
 function react_shortcode($atts){
   extract(shortcode_atts(['id'=> 0], $atts));
-  $js = react_get_manifest($id, 'main.js');
-  $css = react_get_manifest($id, 'main.css');
-  if(!$js || !$css) return;
 
-  wp_enqueue_style('react-style-'.$id, $css);
-  wp_enqueue_script('react-script-'.$id, $js, [], '1.0.0', true);
+  if(!$id) return;
+  $assets = (array)get_post_meta($id, 'assets', true); 
+  $path = get_permalink($id) . 'assets/';
+
+  foreach($assets as $file => $asset){
+    if(preg_match('/\.js$/i', $file)){
+      wp_enqueue_script('react-script-'.md5($id.$file) , $path.$file, [], '1.0.0', true);
+    }
+    if(preg_match('/\.css$/i', $file)){
+      wp_enqueue_style('react-style-'.md5($id.$file) , $path.$file);
+    }
+  }
 
   return '<div id="'. react_get_html_id($id) .'"></div>';
 }
@@ -123,10 +139,7 @@ function react_instruction($post){
     "build": "react-app-rewired build",
     "test": "react-app-rewired test",
     </code> </li>
-    <li>add homepage to package.json to <br><code>
-    "homepage": "'.react_get_build_dir($post->ID, 'baseurl').'",
-    </code> </li>
-    <li> root id: <b>'.react_get_html_id($post->ID).'</b> <br><small>set dom access id in src/index.js and public/index.html</small></li>
+    <li> root id: <code>'.react_get_html_id($post->ID).'</code><small>set dom access id in src/index.js and public/index.html</small></li>
     <li>Build your react-app. <br>
       - <code>npm run build</code><br>
       - zip the build folder<br>
@@ -167,17 +180,19 @@ function react_deploy($postId){
   update_post_meta($postId, 'disabled', isset($_POST['disabled']) ? 1 : 0);
 
   //file build
-  $file = @$_FILES['file'];
-  if(!$file) return;
-  if(!function_exists( 'WP_Filesystem' ) ) {
-    include_once ABSPATH.'/wp-admin/includes/file.php';
-  }
-  WP_Filesystem();
-  
-  $tmp_file = @$file['tmp_name'];
+  $tmp_file = @$_FILES['file']['tmp_name'];
   if(!$tmp_file) return;
-  
-  $path = react_get_build_dir($postId, 'basedir', '');
-  wp_mkdir_p($path);
-  unzip_file( $tmp_file, $path);
+
+  $zip = new ZipArchive;
+  if($zip->open($tmp_file)) {
+      $assets = [];
+      for ($idx = 0; $zipFile = $zip->statIndex($idx); $idx++) { 
+          if ($zipFile['size']) {
+              $contents = $zip->getFromIndex($idx);
+              $key = str_replace('build/', '', $zipFile['name']);
+              $assets[$key] = base64_encode($contents);
+          }
+      }
+      update_post_meta($postId, 'assets', $assets);  
+  }
 }
